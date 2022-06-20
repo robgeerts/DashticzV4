a9b96f3ac2a341e3912b9736f9740a5a20125738
Rob Geerts <rob@wedevise.nl>
2022-06-20 17:04:17 +0200

    LocalStorage

--- src/components/modals/Addblock.vue
+++ src/components/modals/Addblock.vue
@@ -18,15 +18,15 @@
 
                     <div style="max-height:270px;padding-right:10px;overflow:auto">
                         <template v-for="(item, index) in items.all" :key="index">
-                            <div class="d-grid">
+                            <div class="d-grid" v-if="item">
                                 <button type="button" @click="addBlock(index)" class="btn d-flex btn-light text-start rounded mb-5">
                                     <Icon :item=item></Icon>
                                     <div class="flex-grow-1 me-2">
-                                        <span class="fw-bolder text-gray-800 fs-6">{{ item.Name }}</span>
-                                        <span v-if="item.SubType==='Energy'" class="text-muted fw-bold d-block">{{ item.Usage }}</span>
-                                        <span v-else-if="item.SubType==='Gas'" class="text-muted fw-bold d-block">{{ item.CounterToday }}</span>
-                                        <span v-else-if="item.SubType==='RFXMeter counter'" class="text-muted fw-bold d-block">{{ item.CounterToday }}</span>
-                                        <span v-else class="text-muted fw-bold d-block">{{ item.Data }}</span>
+                                        <span class="fw-bolder text-gray-800 fs-6">{{ allitems[item.idx].Name }}</span>
+                                        <span v-if="item.SubType==='Energy'" class="text-muted fw-bold d-block">{{ allitems[item.idx].Usage }}</span>
+                                        <span v-else-if="item.SubType==='Gas'" class="text-muted fw-bold d-block">{{ allitems[item.idx].CounterToday }}</span>
+                                        <span v-else-if="item.SubType==='RFXMeter counter'" class="text-muted fw-bold d-block">{{ allitems[item.idx].CounterToday }}</span>
+                                        <span v-else class="text-muted fw-bold d-block">{{ allitems[item.idx].Data }}</span>
                                     </div>
                                 </button>
                             </div>
@@ -48,7 +48,7 @@ export default defineComponent({
     components: {
         Icon,
     },
-    props: ['items'],
+    props: ['items','allitems'],
     setup(props) {
 
         const addBlock = (id) => {
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,18 +1,28 @@
 <template>
 
-    <Addblock :items=items></Addblock>
+    <!--<Addblock :items=items :allitems=allitems></Addblock>-->
 
   <div class="row mb-5">
       <div class="col-lg-12">
           <div class="card">
               <div class="card-header border-0">
                   <h3 class="card-title fw-bolder text-dark">Dashticz v4</h3>
-                  <button v-if="mode=='view'" type="button" class="btn btn-sm btn-active-color-primary pe-0" @click="switchMode()">
-                      <i class="fa-solid fa-pencil fs-1"></i>
-                  </button>
-                  <button v-if="mode=='edit'" type="button" class="btn btn-sm btn-active-color-primary pe-0" @click="switchMode()">
-                      <i class="fa-solid fa-xmark fs-1"></i>
-                  </button>
+                  <div class="pe-0 pt-5" v-if="mode=='view'">
+                      <button type="button" class="btn btn-sm btn-active-color-primary pe-0" @click="switchMode()">
+                          <i class="fa-solid fa-pencil fs-1"></i>
+                      </button>
+                  </div>
+                  <div class="pe-0 pt-5" v-if="mode=='edit'">
+                      <button type="button" class="btn btn-sm btn-active-color-primary" @click="switchMode()">
+                          <i class="fa-solid fa-check fs-1"></i>
+                      </button>
+                      <button type="button" class="btn btn-sm btn-active-color-primary" @click="switchMode()">
+                          <i class="fa-solid fa-xmark fs-1"></i>
+                      </button>
+                      <button type="button" class="btn btn-sm btn-active-color-primary pe-0" @click="refreshView()">
+                          <i class="fa-solid fa-refresh fs-1"></i>
+                      </button>
+                  </div>
               </div>
           </div>
       </div>
@@ -40,17 +50,28 @@
                     <div
                         class="list-group-item p-0 border-0"
                         v-for="item in items.switches"
-                        :key="item.Name"
-                        :group="{ name: 'blocks', pull: true, put: true }"
-                        :id="`block_${item.idx}`"
+                        :key="item"
+                        :id="`block_${item}`"
                     >
-                        <Switch :item=item :mode=mode></Switch>
+                        {{ allitems[item] }}
                     </div>
+                    <!--
+                    <div
+                        class="list-group-item p-0 border-0"
+                        v-for="item in items.switches"
+                        :key="allitems[item].Name"
+                        :group="{ name: 'blocks', pull: true, put: true }"
+                        :id="`block_${item}`"
+                    >
+                        <Switch :item=allitems[item] :mode=mode></Switch>
+                    </div>-->
                 </draggable>
             </div>
         </div>
     </div>
+      <!--
     <div class="col-lg-4">
+
         <div class="card mb-5">
             <div class="card-header border-0">
                 <h3 class="card-title fw-bolder text-dark">Electriciteit</h3>
@@ -69,7 +90,7 @@
             <div class="card-body p-0 d-flex flex-column">
                 <Chart chart-height="100" chart-color="primary" :mode=mode></Chart>
             </div>
-        </div>
+          </div>
           <div class="card">
               <div class="card-header border-0">
                   <h3 class="card-title fw-bolder text-dark">Gas/Water/Licht</h3>
@@ -91,10 +112,10 @@
                         class="list-group-item p-0 border-0"
                         v-for="item in items.power"
                         :group="{ name: 'blocks', pull: true, put: true }"
-                        :key="item.Name"
-                        :id="`block_${item.idx}`"
+                        :key="item"
+                        :id="`block_${item}`"
                     >
-                        <Energy :item=item :mode=mode></Energy>
+                        <Energy :item=allitems[item] :mode=mode></Energy>
                     </div>
                 </draggable>
             </div>
@@ -122,29 +143,29 @@
                           class="list-group-item p-0 border-0"
                           v-for="item in items.others"
                           :group="{ name: 'blocks', pull: true, put: true }"
-                          :key="item.Name"
-                          :id="`block_${item.idx}`"
+                          :key="item"
+                          :id="`block_${item}`"
                       >
-                          <Temperature :item=item :mode=mode></Temperature>
+                          <Temperature :item=allitems[item] :mode=mode></Temperature>
                       </div>
                   </draggable>
               </div>
           </div>
-      </div>
+      </div> -->
   </div>
 
 </template>
 
 <script lang="ts">
-import {defineComponent, reactive } from "vue";
+import {defineComponent, reactive, ref } from "vue";
 import { VueDraggableNext } from 'vue-draggable-next'
 import axios from "axios";
 
-import Switch from "@/components/types/Switch.vue";
-import Energy from "@/components/types/Energy.vue";
-import Temperature from "@/components/types/Temperature.vue";
-import Chart from "@/components/types/Chart.vue";
-import Addblock from "@/components/modals/Addblock.vue";
+import Switch from "@/components/types/Switch.vue"; /* eslint-disable vue/no-unused-components */
+import Energy from "@/components/types/Energy.vue"; /* eslint-disable vue/no-unused-components */
+import Temperature from "@/components/types/Temperature.vue"; /* eslint-disable vue/no-unused-components */
+import Chart from "@/components/types/Chart.vue"; /* eslint-disable vue/no-unused-components */
+import Addblock from "@/components/modals/Addblock.vue"; /* eslint-disable vue/no-unused-components */
 
 export default defineComponent({
   name: "main-dashboard",
@@ -156,26 +177,18 @@ export default defineComponent({
     Addblock,
     draggable: VueDraggableNext,
   },
-    data(){
-      return {
-          mode:'view'
-      }
-    },
-  methods: {
-    switchMode() {
-        if(this.mode=='view') this.mode='edit';
-        else this.mode='view';
-    }
-  },
   setup() {
 
       const dragging=false;
+      const allitems = reactive({});
       const items = reactive({
-          switches: {},
-          power: {},
-          others: {},
-          all: {},
+          switches: [],
+          power: [],
+          others: [],
+          all: [],
       });
+
+      let mode='view';
 
       const doneMoving = (event) => {
           console.log('doneMoving');
@@ -185,36 +198,58 @@ export default defineComponent({
           console.log('newIndex = '+event.newIndex);
       }
 
-      getDevices(true);
+      getDevices(true,false);
       //setInterval(getDevices, 2000);
 
-      function getDevices(initial) {
+      function getDevices(initial, restart) {
             if(typeof(initial)=='undefined') initial=false;
+            if(typeof(restart)=='undefined') restart=false;
+
             axios.get(process.env.VUE_APP_DOMOTICZ_URL+'/json.htm?type=devices&filter=all&used=true&favorite=0&order=Name&plan=0').then(response => {
 
                 for(var r in response.data.result) {
-
                     var device = response.data.result[r];
+                    allitems[device.idx]=device;
+
+                    items.all[r]=device.idx;
                     if(device.Favorite==1) {
                         if (device.Type == 'Color Switch' || device.Type == 'Light/Switch' || device.Type == 'Lighting 1' || device.Type == 'Lighting 2') {
-                            items.switches[r] = device;
-                        } else if (device.Type == 'RFXMeter' || device.Type == 'P1 Smart Meter') {
-                            device.Data = device.Data.split(';');
-                            items.power[r] = device;
-                        } else {
-                            items.others[r] = device;
+                            items.switches[r] = device.idx;
+                        }
+                        else if (device.Type == 'RFXMeter' || device.Type == 'P1 Smart Meter') {
+                            //device.Data = device.Data.split(';');
+                            //items.power[r] = device.idx;
+                        }
+                        else {
+                           //items.others[r] = device.idx;
                         }
                     }
                     else {
-                      if(initial) items.all[r]=device;
+                      if(initial){
+                          //items.all[r]=device.idx;
+                      }
                     }
                 }
+
+
+                localStorage.setItem('blockItems', JSON.stringify(items));
 
             })
       }
 
+      function switchMode() {
+          if(mode=='view') mode='edit';
+          else mode='view';
+      }
+
+      function refreshView(){
+          if(confirm('Are you sure you want to reset Dashticz and start from scratch?')){
+              getDevices(true,true);
+          }
+      }
+
       return {
-          items,dragging,doneMoving
+          mode,items,allitems,dragging,doneMoving,refreshView,switchMode
       };
 
     }
